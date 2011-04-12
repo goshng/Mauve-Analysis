43e03d303d897d6147f0ece2a2d3638c0555f8ab
Sang Chul Choi <goshng@yahoo.co.kr>
2011-04-12 15:44:58 -0400

    ClonalOrigin's codes are in the repository.

---
+++ src/clonalorigin/moveaddedge.h
@@ -0,0 +1,29 @@
+#ifndef MOVEADDEDGE_H
+#define MOVEADDEDGE_H
+
+#include "move.h"
+
+namespace weakarg
+{
+
+/**
+    @brief This move adds an edge
+*/
+class MoveAddEdge : public Move
+{
+public:
+    MoveAddEdge(Param * p,double a);
+    Move * clone()
+    {
+        return new MoveAddEdge(*this);
+    }
+    int move(vector<int> * samplespace=NULL);
+    inline int move() {
+        return(move(NULL));
+    }
+    ~MoveAddEdge();
+
+};
+
+} // end namespace weakarg
+#endif
